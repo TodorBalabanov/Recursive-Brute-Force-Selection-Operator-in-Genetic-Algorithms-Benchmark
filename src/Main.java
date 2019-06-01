0b1101ff39270c4dc10da5daee71446b10aa039d
Todor Balabanov <todor.balabanov@gmail.com>
2019-06-01 18:14:34 +0300

    Function objects ware added.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,121 @@ import java.util.Random;
  */
 public class Main {
 	/**
+	 * Benchmark function interface.
+	 * 
+	 * @author Todor Balabanov
+	 */
+	private static interface Function {
+		/**
+		 * Calculation of the function result.
+		 * 
+		 * @param input
+		 *            Input vector.
+		 * @return Output value.
+		 */
+		public Double calculate(List<Double> input);
+
+		/**
+		 * Minimal solution space value.
+		 * 
+		 * @return Minimum.
+		 */
+		public double minimum();
+
+		/**
+		 * Maximal solution space value.
+		 * 
+		 * @return Maximum.
+		 */
+		public double maximum();
+
+		/**
+		 * Function name.
+		 * 
+		 * @return Name.
+		 */
+		public String title();
+	}
+
+	/**
+	 * Griewank function.
+	 *
+	 * https://en.wikipedia.org/wiki/Griewank_function
+	 * 
+	 * @author Todor Balabanov
+	 */
+	private static final class Griewank implements Function {
+		@Override
+		public Double calculate(List<Double> x) {
+			double sum = 0D;
+			for (Double xi : x) {
+				sum += (xi * xi);
+			}
+
+			int i = 1;
+			double multiplication = 1D;
+			for (Double xi : x) {
+				multiplication *= Math.cos(xi / Math.sqrt(i));
+				i++;
+			}
+
+			return 1D + sum / 4000D - multiplication;
+		}
+
+		@Override
+		public double minimum() {
+			return -600D;
+		}
+
+		@Override
+		public double maximum() {
+			return +600D;
+		}
+
+		@Override
+		public String title() {
+			return "Griewank";
+		}
+	}
+
+	/**
+	 * Rastrigin function.
+	 *
+	 * https://en.wikipedia.org/wiki/Rastrigin_function
+	 * 
+	 * @author Todor Balabanov
+	 */
+	private static class Rastrigin implements Function {
+		@Override
+		public Double calculate(List<Double> x) {
+			final double A = 10D;
+			final double n = x.size();
+
+			double sum = 0D;
+			for (Double xi : x) {
+				sum += (xi * xi - A * Math.cos(2D * Math.PI * xi));
+			}
+
+			return A * n + sum;
+		}
+
+		@Override
+		public double minimum() {
+			return -5.12D;
+		}
+
+		@Override
+		public double maximum() {
+			return +5.12D;
+		}
+
+		@Override
+		public String title() {
+			return "Rastrigin";
+		}
+	}
+
+	/**
 	 * Pseudo-random number generator.
 	 */
 	private static final Random PRNG = new Random();
@@ -45,69 +160,9 @@ public class Main {
 	private static final int INPUT_SIZE = 100;
 
 	/**
-	 * Input minimum value.
-	 */
-	// private static final double INPUT_MINIMUM = -5.12D;
-	private static final double INPUT_MINIMUM = -600D;
-
-	/**
-	 * Input maximum value;
-	 */
-	// private static final double INPUT_MAXIMUM = +5.12D;
-	private static final double INPUT_MAXIMUM = +600D;
-
-	/**
 	 * Counting of generated individuals.
 	 */
 	private static long individuals = 0L;
-
-	/**
-	 * Griewank function.
-	 *
-	 * https://en.wikipedia.org/wiki/Griewank_function
-	 *
-	 * @param x
-	 *            Input vector (-600<=xi<=+600).
-	 * 
-	 * @return Result of the function.
-	 */
-	private static Double griewank(List<Double> x) {
-		double sum = 0D;
-		for (Double xi : x) {
-			sum += (xi * xi);
-		}
-
-		int i = 1;
-		double multiplication = 1D;
-		for (Double xi : x) {
-			multiplication *= Math.cos(xi / Math.sqrt(i));
-			i++;
-		}
-
-		return 1D + sum / 4000D - multiplication;
-	}
-
-	/**
-	 * Rastrigin function.
-	 *
-	 * https://en.wikipedia.org/wiki/Rastrigin_function
-	 *
-	 * @param x
-	 *            Input vector (-5.12<=xi<=+5.12).
-	 * 
-	 * @return Result of the function.
-	 */
-	private static Double rastrigin(List<Double> x) {
-		final double A = 10D;
-		final double n = x.size();
-
-		double sum = 0D;
-		for (Double xi : x) {
-			sum += (xi * xi - A * Math.cos(2D * Math.PI * xi));
-		}
-
-		return A * n + sum;
-	}
 
 	/**
 	 * Uniform crossover.
@@ -157,15 +212,18 @@ public class Main {
 	 *            Recursion depth.
 	 * @param size
 	 *            Population size size.
+	 * @param function
+	 *            Benchmark function object reference.
 	 * @return Best found solution.
 	 */
-	private static List<Double> solution(int depth, int size) {
+	private static List<Double> solution(int depth, int size,
+			Function function) {
 		List<Double> result = new ArrayList<Double>();
 
 		if (depth > 0) {
 			List<List<Double>> population = new ArrayList<List<Double>>();
 			for (int j = 0; j < size; j++) {
-				population.add(solution(depth - 1, size));
+				population.add(solution(depth - 1, size, function));
 			}
 
 			/* Crossover and mutation with each other. */
@@ -175,9 +233,8 @@ public class Main {
 					List<Double> child = crossover(first, second);
 					mutate(child);
 
-					// double value = rastrigin(child);
-					double value = griewank(child);
-					
+					double value = function.calculate(child);
+
 					/* Keep track of the best solution found. */
 					if (value < optimum) {
 						result = child;
@@ -191,45 +248,57 @@ public class Main {
 		} else if (depth == 0) {
 			/* First generation is selected randomly. */
 			for (int i = 0; i < INPUT_SIZE; i++) {
-				result.add(INPUT_MINIMUM
-						+ PRNG.nextDouble() * (INPUT_MAXIMUM - INPUT_MINIMUM));
+				result.add(function.minimum() + PRNG.nextDouble()
+						* (function.maximum() - function.minimum()));
 			}
 		}
 
 		return result;
 	}
 
+	/**
+	 * Application single entry point.
+	 * 
+	 * @param args
+	 *            Command line arguments.
+	 */
 	public static void main(String[] args) {
-		for (int depth = MIN_RECURSION_DEPTH; depth <= MAX_RECURSION_DEPTH; depth++) {
-			for (int size = MIN_POPULATION_SIZE; size <= MAX_POPULATION_SIZE; size++) {
-				individuals = 0L;
-				long start = System.currentTimeMillis();
-				List<Double> input = solution(depth, size);
-				long stop = System.currentTimeMillis();
+		Function functions[] = {new Rastrigin(), new Griewank()};
 
-				// double output = rastrigin(input);
-				double output = griewank(input);
+		for (Function function : functions) {
+			for (int depth = MIN_RECURSION_DEPTH; depth <= MAX_RECURSION_DEPTH; depth++) {
+				for (int size = MIN_POPULATION_SIZE; size <= MAX_POPULATION_SIZE; size++) {
+					individuals = 0L;
+					long start = System.currentTimeMillis();
+					List<Double> input = solution(depth, size, function);
+					long stop = System.currentTimeMillis();
 
-				System.out.print("Recursion Depth:");
-				System.out.print("\t");
-				System.out.println(depth);
-				System.out.print("Population Size:");
-				System.out.print("\t");
-				System.out.println(size);
-				System.out.print("Evaluated Individuals:");
-				System.out.print("\t");
-				System.out.println(individuals);
-				System.out.print("Input Vector:");
-				System.out.print("\t");
-				System.out.println(input.toString().replace(',', '\t')
-						.replace("[", "").replace("]", ""));
-				System.out.print("Output Value:");
-				System.out.print("\t");
-				System.out.println(output);
-				System.out.print("Time [ms]:");
-				System.out.print("\t");
-				System.out.println(stop - start);
-				System.out.println();
+					double output = function.calculate(input);
+
+					System.out.print("Fuction Name:");
+					System.out.print("\t");
+					System.out.println(function.title());
+					System.out.print("Recursion Depth:");
+					System.out.print("\t");
+					System.out.println(depth);
+					System.out.print("Population Size:");
+					System.out.print("\t");
+					System.out.println(size);
+					System.out.print("Evaluated Individuals:");
+					System.out.print("\t");
+					System.out.println(individuals);
+					System.out.print("Input Vector:");
+					System.out.print("\t");
+					System.out.println(input.toString().replace(',', '\t')
+							.replace("[", "").replace("]", ""));
+					System.out.print("Output Value:");
+					System.out.print("\t");
+					System.out.println(output);
+					System.out.print("Time [ms]:");
+					System.out.print("\t");
+					System.out.println(stop - start);
+					System.out.println();
+				}
 			}
 		}
 	}
